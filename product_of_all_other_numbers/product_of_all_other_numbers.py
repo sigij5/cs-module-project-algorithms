9f84bdbf1f3b5a6b12049bd5723368b4ac1e26a0
Sigi Jaeckel <sigij55@gmail.com>
2020-09-16 19:20:30 -0400

    completed sliding windows

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,28 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # i = 0
+    # return_array = []
+    # while i < len(arr):
+    if len(arr) == 1:
+        print(0)
+        return
+    left = [0] * len(arr)
+    right = [0] * len(arr)
+    products = [0] * len(arr)
 
-    pass
+    left[0] = 1
+    right[len(arr)-1] = 1
+
+    for i in range(1, len(arr)):
+        left[i] = arr[i-1] * left[i-1]
+    for j in range(len(arr)-2, -1, -1):
+        right[j] = arr[j+1] * right[j+1]
+    for i in range(len(arr)):
+        products[i] = left[i] * right[i]
+        
+
+    return products
 
 
 if __name__ == '__main__':
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,10 +3,19 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    max = 0
+    max_list = []
+    # while i <= len(nums) - k:
+    #     window = arr[i:]
 
-    pass
-
+    # pass
+    for i in range(len(nums) - k + 1):
+        max = nums[i]
+        for j in range(1, k):
+            if nums[i + j] > max:
+                max = nums[i + j]
+        max_list.append(max)
+    return max_list
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
