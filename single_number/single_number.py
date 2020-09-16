509e11a278347c2d576bad9d12d542373e33d17f
Sigi Jaeckel <sigij55@gmail.com>
2020-09-16 13:41:27 -0400

    completed single_number and moving_zeroes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,15 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    arr.sort()
+    i = 0
+    while i < len(arr) - 1:
+        if arr[i] == arr[i+1]:
+            arr.pop(i+1)
+            arr.pop(i)
+        else:
+            i+=1
+    return arr[0]
 
 
 if __name__ == '__main__':
