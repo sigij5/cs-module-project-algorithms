e97a8f3fb272d8fa1796e2e79a2d3deac183dcf5
Sigi Jaeckel <sigij55@gmail.com>
2020-09-17 00:49:10 -0400

    completed cookies with small input

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,19 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    if n == 0:
+        return 1  # test wanted this to equal zero?
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    # if n == 3:
+    #     return 4
+    
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-    pass
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
