b104f6cfd1241e46ad678ed392d9ededb91e903c
Sigi Jaeckel <sigij55@gmail.com>
2020-09-17 12:24:26 -0400

    completed mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,19 +2,27 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    if n == 0:
-        return 1  # test wanted this to equal zero?
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    # if n == 3:
-    #     return 4
+# def eating_cookies(n):
+#     if n == 0:
+#         return 1  # test wanted this to equal zero?
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 2
     
-    else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+#     else:
+#         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
+def eating_cookies(n, mem):
+    if n == 0:
+        return 1
+    if n < 3:
+        return n
+    elif mem[n] > 0:
+        return mem[n]
+    else:
+        mem[n] = eating_cookies(n-1, mem) + eating_cookies(n-2, mem) + eating_cookies(n-3, mem)
+        return mem[n]
 
 
 if __name__ == "__main__":
